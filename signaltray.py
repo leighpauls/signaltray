2be36d8b728c15223c505ea307bbeced97dae88f
Leigh Pauls <leighpauls@fb.com>
2014-08-22 19:04:16 -0700

    Use trailing average of many single-ping calls to shell so that the icon can respond to changes in connection state faster

--- signaltray.py
+++ signaltray.py
@@ -2,6 +2,7 @@ import objc, re, os
 from Foundation import *
 from AppKit import *
 from PyObjCTools import NibClassBuilder, AppHelper
+import numpy
 
 import ping
 
@@ -16,26 +17,9 @@ SLOW_STATE_ID = 1
 BAD_STATE_ID = 2
 DISCONNECTED_STATE_ID = 3
 
+TRAILING_AVERAGE_COUNT = 5
+
 start_time = NSDate.date()
-
-def evaluatePing(ping_results):
-    """Evaluates the results of AsyncPing, returning the *_STATE_ID appropriate
-    to these results"""
-    # TODO: these benchmarks are highly subjective, and should be user-tunable
-    if ping_results is None or ping_results.loss_pct > 99:
-        return DISCONNECTED_STATE_ID
-    # a rough estimate of the 95th percentile round trip ping
-    p95_ping = ping_results.round_trip + ping_results.stddev * 3
-    # 100ms is the magic number for unnoticable latency
-    if ping_results.loss_pct < 25 and p95_ping < 100:
-        return GOOD_STATE_ID
-    # these numbers are arbitrary
-    if ping_results.loss_pct < 25 and p95_ping < 300:
-        return SLOW_STATE_ID
-    return BAD_STATE_ID
-
-def makePing():
-    return ping.AsyncPing('google.com')
 
 class Signaltray(NSObject):
     def applicationDidFinishLaunching_(self, notification):
@@ -75,7 +59,8 @@ class Signaltray(NSObject):
         self.status_item.setMenu_(menu)
 
         # Start the first asynchronous ping process
-        self.ping = makePing()
+        self.trailing_pings = []
+        self.cur_ping = makePing()
 
         # Get the timer going
         self.timer = NSTimer.alloc() \
@@ -92,23 +77,65 @@ class Signaltray(NSObject):
         self.timer.fire()
 
     def tick_(self, notification):
-        if not self.ping.isReady():
+        # no new information
+        if not self.cur_ping.isReady():
             return
-        ping_results = self.ping.getResults()
 
-        self.status_item.setImage_(
-            self.state_images[evaluatePing(ping_results)])
+        image_idx, status_string = self.evaluatePings(
+            self.cur_ping.getResults())
 
-        status_string = 'No connection' if ping_results is None else '\n'.join([
-            "Packet Loss: " + str(int(ping_results.loss_pct)) + "%",
-            "Ping: " + str(int(ping_results.round_trip)) + " ms",
-            "Ping stddev: " + str(int(ping_results.stddev)) + " ms"])
+        self.status_item.setImage_(self.state_images[image_idx])
         self.status_item.setToolTip_(status_string)
         self.status_menu_item.setAttributedTitle_(
             NSAttributedString.alloc().initWithString_(status_string))
 
         # start a new ping
-        self.ping = makePing()
+        self.cur_ping = makePing()
+
+    def evaluatePings(self, new_ping_results):
+        """updates the rolling average ping states then returns
+        (state_image_idx, status_string) """
+        if new_ping_results is None:
+            # total disconnect, past results are now irrelevant
+            self.trailing_pings = []
+            return (DISCONNECTED_STATE_ID, 'No connection')
+
+        # update the trailing pings
+        self.trailing_pings.append(new_ping_results)
+        if len(self.trailing_pings) > TRAILING_AVERAGE_COUNT:
+            self.trailing_pings.pop(0)
+
+        # take the average stats of the trailing pings
+        loss_pct = numpy.mean([p.loss_pct for p in self.trailing_pings])
+        round_trips = [p.round_trip for p in self.trailing_pings]
+        ping_mean = numpy.mean(round_trips)
+        ping_stddev = numpy.std(round_trips)
+
+        image_idx = evaluatePing(loss_pct, ping_mean, ping_stddev)
+        status_string = '\n'.join([
+            "Packet Loss: " + str(int(loss_pct)) + "%",
+            "Ping: " + str(int(ping_mean)) + " ms",
+            "Ping stddev: " + str(int(ping_stddev)) + " ms"])
+        return (image_idx, status_string)
+
+def evaluatePing(loss_pct, ping_mean, ping_stddev):
+    """Evaluates the results of AsyncPing, returning the *_STATE_ID appropriate
+    to these results"""
+    if loss_pct > 99:
+        return DISCONNECTED_STATE_ID
+    # TODO: these benchmarks are highly subjective, and should be user-tunable
+    # a rough estimate of the 95th percentile round trip ping
+    p95_ping = ping_mean + ping_stddev * 3
+    # 100ms is the magic number for unnoticable latency
+    if loss_pct < 25 and p95_ping < 100:
+        return GOOD_STATE_ID
+    # these numbers are arbitrary
+    if loss_pct < 25 and p95_ping < 300:
+        return SLOW_STATE_ID
+    return BAD_STATE_ID
+
+def makePing():
+    return ping.AsyncPing('google.com', num_pings=1)
 
 if __name__ == "__main__":
     app = NSApplication.sharedApplication()
